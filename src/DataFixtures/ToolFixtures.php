ea9d9e61fb01e4aa2cc81f5beb027e4206069fe4
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-02-06 10:39:45 +0100

    add: génération de la fixture de l'entité Tool avec un fichier Json pour les données

---
+++ src/DataFixtures/ToolFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\ToolCategoryFactory;
+use App\Factory\ToolFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ToolFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $tools = json_decode(file_get_contents(__DIR__.'/data/Tool.json'), true);
+        foreach ($tools as $tool) {
+            ToolFactory::createOne([
+                    'name' => $tool['name'],
+                    'toolCategory' => ToolCategoryFactory::findOrCreate([
+                        'name' => $tool['toolCategory'],
+                    ])]
+            );
+        }
+    }
+
+    public function getDependencies(): array
+    {
+        return [ToolCategoryFixtures::class];
+    }
+}
