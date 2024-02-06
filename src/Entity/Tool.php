0fd4f4bf3358e08b2f4dbf215defd45206b43380
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-02-06 10:37:59 +0100

    add: génération de l'entité Tool et de son repository

---
+++ src/Entity/Tool.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ToolRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ToolRepository::class)]
+class Tool
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: Recipe::class, mappedBy: 'tools')]
+    private Collection $recipes;
+
+    #[ORM\ManyToOne(inversedBy: 'tools')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ToolCategory $toolCategory = null;
+
+    public function __construct()
+    {
+        $this->recipes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Recipe>
+     */
+    public function getRecipes(): Collection
+    {
+        return $this->recipes;
+    }
+
+    public function addRecipe(Recipe $recipe): static
+    {
+        if (!$this->recipes->contains($recipe)) {
+            $this->recipes->add($recipe);
+            $recipe->addTool($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipe(Recipe $recipe): static
+    {
+        if ($this->recipes->removeElement($recipe)) {
+            $recipe->removeTool($this);
+        }
+
+        return $this;
+    }
+
+    public function getToolCategory(): ?ToolCategory
+    {
+        return $this->toolCategory;
+    }
+
+    public function setToolCategory(?ToolCategory $toolCategory): static
+    {
+        $this->toolCategory = $toolCategory;
+
+        return $this;
+    }
+}
