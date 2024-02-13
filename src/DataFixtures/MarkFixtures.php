b430749e007abcceba240c10954cd25036c05678
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2024-02-13 10:17:18 +0100

    fix: add user and recipe fields with dependency

--- src/DataFixtures/MarkFixtures.php
+++ src/DataFixtures/MarkFixtures.php
@@ -3,13 +3,29 @@
 namespace App\DataFixtures;
 
 use App\Factory\MarkFactory;
+use App\Factory\RecipeFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class MarkFixtures extends Fixture
+class MarkFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        MarkFactory::createMany(10);
+        MarkFactory::createMany(10, function () {
+            return [
+                'user' => UserFactory::random(),
+                'recipe' => RecipeFactory::random(),
+            ];
+        });
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+            RecipeFixtures::class,
+        ];
     }
 }
