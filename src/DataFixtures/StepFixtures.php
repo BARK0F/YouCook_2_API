9c7952d053b345502395cd0f5ccfa8606972f22c
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-02-13 08:55:33 +0100

    Ajout de creation de 5 StepFixtures via les factory

--- src/DataFixtures/StepFixtures.php
+++ src/DataFixtures/StepFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\StepFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,6 @@ class StepFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $manager->flush();
+        StepFactory::createMany(5);
     }
 }
