7860b6d1c509a924aa69c8fe21a459d091f86193
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2024-02-20 09:42:57 +0100

    test: add recipe get cest

---
+++ tests/Api/Recipe/RecipeGetCest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests\Api\Recipe;
+
+use App\Entity\Recipe;
+use App\Factory\RecipeFactory;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
+class RecipeGetCest
+{
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'name' => 'string',
+            'difficulty' => 'string',
+            'description' => 'string',
+            'nbPeople' => 'integer',
+            'nbDay' => 'integer',
+            'nbHour' => 'integer',
+            'nbMinute' => 'integer',
+        ];
+    }
+
+    public function userCanFetchAllRecipes(ApiTester $I)
+    {
+        $user = UserFactory::createOne()->object();
+
+        $data = [
+            'name' => 'Couscous',
+            'difficulty' => 'medium',
+            'description' => 'Un magnifique couscous de test',
+            'nbPeople' => 2,
+            'nbDay' => 0,
+            'nbHour' => 3,
+            'nbMinute' => 45,
+        ];
+
+        RecipeFactory::createOne([
+            'name' => 'Couscous',
+            'difficulty' => 'medium',
+            'description' => 'Un magnifique couscous de test',
+            'nbPeople' => 2,
+            'nbDay' => 0,
+            'nbHour' => 3,
+            'nbMinute' => 45,
+            'author' => $user,
+        ]);
+
+        $I->sendGet('/api/recipes/1');
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Recipe::class, '/api/recipes/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
+    }
+}
