6ee865865f74ff3b43de3a4cb48299aa44b42a05
mekh0006 <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-02-20 10:20:26 +0100

    add: ajout de l'entité Tool dans les ressources de l'api

--- src/Entity/Tool.php
+++ src/Entity/Tool.php
@@ -2,20 +2,29 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ToolRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource]
+#[Get(normalizationContext: ['groups' => ['Tool_read']])]
+#[GetCollection(normalizationContext: ['groups' => ['Tool_read']])]
 #[ORM\Entity(repositoryClass: ToolRepository::class)]
 class Tool
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('Tool_read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 20)]
+    #[Groups('Tool_read')]
     private ?string $name = null;
 
     #[ORM\ManyToMany(targetEntity: Recipe::class, mappedBy: 'tools')]
@@ -23,6 +32,7 @@ class Tool
 
     #[ORM\ManyToOne(inversedBy: 'tools')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups('Tool_read')]
     private ?ToolCategory $toolCategory = null;
 
     public function __construct()
