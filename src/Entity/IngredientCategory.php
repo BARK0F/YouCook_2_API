a8c0b184c361b5d9489f92df83e754f7a6105b5d
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-03-05 10:04:48 +0100

    ajout de l'apiRessource et des groupes Get

--- src/Entity/IngredientCategory.php
+++ src/Entity/IngredientCategory.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use App\Repository\IngredientCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: IngredientCategoryRepository::class)]
+#[ApiResource(order: ['name' => 'ASC'])]
+#[Get(normalizationContext: ['groups' => ['get_IngredientCategory', 'user_IngredientCategory']])]
 class IngredientCategory
 {
     #[ORM\Id]
