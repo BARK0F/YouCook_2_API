8cd9c551ed7d840f6ce57c7ae4279b19b729e315
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-03-29 17:23:02 +0100

    rebase: ajout des attributs picture, category, allergen et constitutes dans les données des tests de l'opération Get de l'entité Ingrédient

--- tests/Api/Ingredient/IngredientGetCest.php
+++ tests/Api/Ingredient/IngredientGetCest.php
@@ -3,6 +3,9 @@
 namespace App\Tests\Api\Ingredient;
 
 use App\Entity\Ingredient;
+use App\Factory\AllergenFactory;
+use App\Factory\ConstituteFactory;
+use App\Factory\IngredientCategoryFactory;
 use App\Factory\IngredientFactory;
 use App\Tests\Support\ApiTester;
 
@@ -13,6 +16,10 @@ class IngredientGetCest
         return [
             'id' => 'integer',
             'name' => 'string',
+            'picture' => 'string',
+            'category' => 'array',
+            'allergen' => 'array',
+            'constitutes' => 'array',
         ];
     }
 
@@ -21,6 +28,10 @@ class IngredientGetCest
         // 1. 'Arrange'
         $data = [
             'name' => 'clementine',
+            'picture' => '',
+            'category' => IngredientCategoryFactory::createOne(),
+            'allergen' => AllergenFactory::createOne(),
+            'constitutes' => ConstituteFactory::createOne(),
         ];
         IngredientFactory::createOne($data);
 
